cd6ccd8da68217eaaa67f544afd45a11411e3572
irisdiakoumi <iridiumcodes@gmail.com>
2022-05-07 01:10:53 +0300

    QAPair Model added

---
+++ QuestionsAsked/Models/QAPair.cs
@@ -0,0 +1,14 @@
+﻿namespace QuestionsAsked.Models
+{
+    public class QAPair
+    {
+        public int Id { get; set; }
+        public string Question { get; set; }
+        public string Answer { get; set; }
+
+        public QAPair()
+        {
+
+        }
+    }
+}
